3917a80c32d977dc785581164b3d5ed5068f0a2c
dangnh <dang.nh.aprotrain@gmail.com>
2020-09-04 20:03:35 +0700

    init oauth2-google module

--- web/rest-api/src/main/java/jp/afterfit/core/api/ApiApplication.java
+++ web/rest-api/src/main/java/jp/afterfit/core/api/ApiApplication.java
@@ -12,13 +12,13 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 @Slf4j
 public class ApiApplication extends SpringBootServletInitializer implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class, args);
+    }
 
-	@Override
-	public void run(String... args) {
+    @Override
+    public void run(String... args) {
 
-	}
+    }
 
 }
--- web/rest-api/src/main/java/jp/afterfit/core/api/exception/ExceptionControllerAdvice.java
+++ web/rest-api/src/main/java/jp/afterfit/core/api/exception/ExceptionControllerAdvice.java
@@ -1,14 +1,13 @@
 package jp.afterfit.core.api.exception;
 
-import jp.afterfit.core.framework.exception.handler.IExceptionHandler;
 import jp.afterfit.core.framework.exception.DefaultRestExceptionControllerAdvice;
+import jp.afterfit.core.framework.exception.handler.IExceptionHandler;
 import jp.afterfit.core.framework.exception.type.ServiceException;
 import org.springframework.validation.BindException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 
 @ControllerAdvice
 public class ExceptionControllerAdvice extends DefaultRestExceptionControllerAdvice {
-
 
     public ExceptionControllerAdvice(IExceptionHandler<ServiceException> serviceExceptionHandler,
                                      IExceptionHandler<BindException> bindExceptionHandler) {
